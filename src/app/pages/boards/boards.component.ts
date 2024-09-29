ced94e5c0f0542c4970cda2e01d9312c7bc9e78d
JessCP25 <jpayanoc25@gmail.com>
2024-09-29 18:44:08 -0500

    Agregando iconos

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faBox,
+  faWaveSquare,
+  faClock,
+} from '@fortawesome/free-solid-svg-icons';
+import { faTrello } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent],
-  templateUrl: './boards.component.html'
+  imports: [NavbarComponent, FontAwesomeModule],
+  templateUrl: './boards.component.html',
 })
 export class BoardsComponent {
-
+  faClock = faClock;
+  faBox = faBox;
+  faWaveSquare = faWaveSquare;
+  faTrello = faTrello;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
       colors: {
         success: colors.green,
         primary: colors.blue,
+        danger: colors.red
+      },
+      container: {
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1024px',
+          '2xl': '1536px'
+        }
       }
     },
   },
